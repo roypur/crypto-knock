120c1c2b337ad2b5eca103217b93f608d37a0dd3
Roy Olav Purser <admin@royolav.net>
2015-02-09 13:59:53 +0100

    upload

---
+++ send.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import socket
+import json
+import hmac
+
+
+ip = "127.0.0.1"
+state = "close"
+
+
+key = "kjhnbskdfghntg"
+
+address = "127.0.0.1"
+port = 5005
+
+
+signature = hmac.new(key.encode("utf-8"), ip.encode('utf-8') + state.encode('utf-8'), 'sha512').hexdigest()
+
+data = {}
+data['ip'] = ip
+data['state'] = state;
+data['signature'] = signature
+
+
+
+message = json.dumps(data)
+
+packet = message.encode("utf-8")
+
+print("UDP target IP:", address)
+print("UDP target port:", port)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.sendto(packet, (address, port))
---
+++ server.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+import socket
+import json
+import hmac
+import iptc
+
+address = "127.0.0.1"
+port = 5005
+
+key = "kjhnbskdfghntg"
+
+def addIP(ip):
+    rule = iptc.Rule()
+    rule.src = ip
+    rule.create_target("ACCEPT")
+    
+    match = rule.create_match("comment")
+    match.comment = "port-knock"
+    
+    chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
+    chain.insert_rule(rule)
+
+def removeIP(ip):
+    
+    run = True
+    
+    while run:
+        try:
+            rule = iptc.Rule()
+            rule.src = ip
+            rule.create_target("ACCEPT")
+    
+            match = rule.create_match("comment")
+            match.comment = "port-knock"
+    
+            chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
+            chain.delete_rule(rule)
+        except:
+            run = False
+
+
+
+def udpServer():
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((address, port))
+
+    while True:
+        try:
+            data = sock.recv(256)
+            try:
+                content = json.loads(data.decode("utf-8"))
+            
+                checkSum = hmac.new(key.encode("utf-8"), content['ip'].encode("utf-8") + content['state'].encode('utf-8'), 'sha512')
+            
+                signature = checkSum.hexdigest()
+            
+                success = hmac.compare_digest(signature, content['signature'])
+    
+                if success:
+                    print("success", content['ip'])
+                    try:
+                        
+                        if content['state'] == "open":
+                            addIP(content['ip'])
+                        elif content['state'] == "close":
+                            removeIP(content['ip'])
+                            
+                    except Exception as e:
+                        print(str(e))
+            except:
+                print("invalid package")
+        
+        except:
+            print("\n")
+            exit()
+
+udpServer()
+
